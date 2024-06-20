d4aa8356bf008945e1cd77cc2f8f9e9f469c4d95
Marc-AntoineMarie <mmarie.galaxie@gmail.com>
2024-06-20 09:38:14 +0200

    pute

--- back.js
+++ back.js
@@ -10,7 +10,7 @@ function isLowerthan (value) {
     return parseFloat(value) < 999999999999;
 }
 
-function nombre (value) {
+function isValidNumber (value) {
     return Number.isInteger(value)
 }
 
@@ -20,44 +20,14 @@ function isValidFloat(value) {
 
 function verifamount(value) {
 
-
-    // if (isEmpty(value)) {
-    //     document.getElementById("error_txt").textContent = "";
-    // }
-    // else{
-    //     document.getElementById("error_txt").textContent = "Montant emprunté";
-    // }
-
-    // if (isGreaterthan(value)) {
-    //     document.getElementById("error_txt").textContent = "";
-    // }
-    // else{
-    //     document.getElementById("error_txt").textContent = "Montant emprunté";
-    // }
-
-    // if (isLowerthan(value)) {
-    //     document.getElementById("error_txt").textContent = "";
-    // }
-    // else{
-    //     document.getElementById("error_txt").textContent = "Montant emprunté";
-    // }
-
-    // if (nombre (value)) {
-    //     document.getElementById("error_txt").textContent = "";
-    // }
-    // else{
-    //     document.getElementById("error_txt").textContent = "Montant emprunté";
-    // }
-
-    if (isValidFloat(value)) {
-        document.getElementById("error_txt").textContent = "Montant emprunté";
+    if (isEmpty(value) && isGreaterthan(value) && isLowerthan(value) && isValidNumber (value) && isValidFloat(value)) {
+        return true
     }
-    else{
-        document.getElementById("error_txt").textContent = "";
+    else {
+        return false
     }
+
 }
-
-
 
 const number = document.querySelector("#calculator");
 number.addEventListener("click", function (event) {
@@ -74,75 +44,9 @@ number.addEventListener("click", function (event) {
     let parsechampnd = parseInt(ndchamp);
     let parsechampth = parseInt(thchamp);
 
-    console.log(verifamount(stchamp));
+    console.log(verifamount(parsechampst));
   
-
 });
 
 
-
-
-
-
-
-
-
-
-// const number = document.querySelector("#calculator");
-
-// number.addEventListener("click", function (event) {
-
-//     event.preventDefault();
-
-//     let stchamp =  document.querySelector("#amount").value;
-//     let ndchamp =  document.querySelector("#level").value;
-//     let thchamp =  document.querySelector("#time").value;
-
-
-//     // Initialise le message d'erreur
-//     let errorMessage = "Veuillez remplir les champs :";
-//     let isError = false;
-
-//     // Vérifie chaque champ
-//     if (!stchamp < 10000 || stchamp > 999999999999) {
-//         errorMessage += " Montant emprunté";
-//         isError = true;
-//     }
-//     if (!ndchamp < 0.2 || ndchamp > 15) {
-//         errorMessage += " Taux nominal";
-//         isError = true;
-//     }
-//     if (!thchamp) {
-//         errorMessage += " Durée de rembourseme";
-//         isError = true;
-//     }
-//     if (!thchamp || thchamp == "" || ndchamp == "" || stchamp == "") {  
-//         errorMessage
-//         isError = true;
-//     }
-
-
-//     // Affiche le message d'erreur s'il y a des champs non remplis
-//     if (isError) {
-//         document.getElementById('errorMessage').innerText = errorMessage;
-//     } else {
-//         document.getElementById('errorMessage').innerText = "";
-//     }
-
-
-
-
-//     // if (stchamp.trim() == "") {
-//     //     document.getElementById("error_txt").textContent = "Veuillez remplir tous les champs !";
-//     // }
-//     // else if (ndchamp.trim() == ""){
-//     //     document.getElementById("error_txt").textContent = "Veuillez remplir tous les champs !";
-//     // }   
-//     // else if (thchamp.trim() == ""){
-//     //     document.getElementById("error_txt").textContent = "Veuillez remplir tous les champs !";
-//     // }   
-//     // else {
-//     //     document.getElementById("error_txt").textContent = "";
-//     //}
-// });
 
