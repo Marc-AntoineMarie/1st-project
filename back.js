f522993d6b9a53fdba74f1d65abe0ae25dde2814
Marc-AntoineMarie <mmarie.galaxie@gmail.com>
2024-06-21 14:38:26 +0200

    z'tr

--- back.js
+++ back.js
@@ -6,22 +6,28 @@ function isGreaterthan (value) {
     return parseFloat(value) >= 10000;
 }
 
+function isGreaterthanforlevel (value) {
+    return parseFloat(value) >= 0.1;
+}
+
+function isGreaterthanfortime(value) {
+    return parseFloat(value) >= 5;
+}
+
 function isLowerthan (value) {
     return parseFloat(value) < 999999999999;
 }
 
-// function isValidNumber (value) {
-//     return Number.isInteger((value))
-// }  
+function isLowerthanforlevel (value) {
+    return parseFloat(value) < 15;
+}
 
+function isLowerthanfortime (value) {
+    return parseFloat(value) < 30;
+}
 
 function isValidInt(value) {
-    if (!isNaN(parseInt(value, 10)) && typeof value !== 'string') {
-        return true;
-    } 
-    else {
-        return false;
-    }
+    return (value % 1 == 0);
 }
 
 function isValidFloat(value) {
@@ -30,7 +36,7 @@ function isValidFloat(value) {
 
 function verifamount(value) {
 
-    if (isEmpty(value) && isGreaterthan(value) && isLowerthan(value) && isValidInt(value) && !isValidFloat(value)) {
+    if (isEmpty(value) && isGreaterthan(value) && isLowerthan(value) && isValidInt(value)) {
         return true
     }
     else {
@@ -38,9 +44,25 @@ function verifamount(value) {
     }
 }
 
+function veriflevel(value) {
 
-// console.log(verifamount(10000));
+    if (isEmpty(value && isGreaterthanforlevel(value) && isLowerthanforlevel(value) && isValidFloat(value))) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
+function veriftime(value) {
+
+    if (isEmpty(value) && isLowerthanfortime(value) && isGreaterthanfortime(value) && isValidInt(value)) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
 
 const number = document.querySelector("#calculator");
@@ -53,18 +75,28 @@ number.addEventListener("click", function (event) {
     let ndchamp =  document.querySelector("#level").value;
     let thchamp =  document.querySelector("#time").value;
 
+    verifamount(stchamp).errorMessagess
+    veriflevel(ndchamp).errorMessage
+    veriftime(thchamp).errorMessage
 
-    let parsechampst = parseInt(stchamp);
-    let parsechampnd = parseInt(ndchamp);
-    let parsechampth = parseInt(thchamp);
+    let errors = [];
 
-    // let type = typeof parsechampst
+    if (!verifamount(stchamp)) {
+        errors.push("amount");
+    }
+    if (!veriflevel(ndchamp)) {
+        errors.push("level");
+    }
+    if (!veriftime(thchamp)) {
+        errors.push("time");
+    }
 
-    // console.log(type)
-
-    // console.log(parsechampst)
-
-    console.log(verifamount(parsechampst));
+    if (errors.length > 0) {
+        errorMessage += " " + errors.join(", ");
+        alert(errorMessage);
+    } else {
+        alert("Tous les champs sont correctement remplis.");
+    }
 });
 
 
